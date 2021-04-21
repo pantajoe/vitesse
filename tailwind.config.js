949824fa3bfc622b1877270787bbc7d2d17ff7f1
Joe Pantazidis <joe.pantazidis@outlook.de>
2021-04-21 09:23:18 +0200

    update deps replaced:   - pnpm -> yarn   - windicss -> tailwindcss removed:   - markdown support   - pwa plugin   - prism themes added:   - headlessui

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class', // or 'media' or false
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,104 +1,64 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
-import Layouts from 'vite-plugin-vue-layouts'
-import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
-import ViteComponents from 'vite-plugin-components'
-import Markdown from 'vite-plugin-md'
-import WindiCSS from 'vite-plugin-windicss'
-import { VitePWA } from 'vite-plugin-pwa'
-import VueI18n from '@intlify/vite-plugin-vue-i18n'
-import Prism from 'markdown-it-prism'
+import path from 'path';
+// Import nothing from vite-ssg to extend vite config (see `ssgOptions`)
+import {} from 'vite-ssg';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import pages from 'vite-plugin-pages';
+import layouts from 'vite-plugin-vue-layouts';
+import viteComponents from 'vite-plugin-components';
+import viteIcons, { ViteIconsResolver } from 'vite-plugin-icons';
+import vueI18n from '@intlify/vite-plugin-vue-i18n';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
+
   plugins: [
-    Vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
+    vue(),
 
     // https://github.com/hannoeru/vite-plugin-pages
-    Pages({
-      extensions: ['vue', 'md'],
-    }),
+    pages(),
 
     // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
-    Layouts(),
+    layouts(),
 
-    // https://github.com/antfu/vite-plugin-md
-    Markdown({
-      wrapperClasses: 'prose prose-sm m-auto text-left',
-      headEnabled: true,
-      markdownItSetup(md) {
-        // https://prismjs.com/
-        md.use(Prism)
-      },
-    }),
+    // https://github.com/antfu/vite-plugin-icons
+    viteIcons(),
 
     // https://github.com/antfu/vite-plugin-components
-    ViteComponents({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ['vue', 'md'],
-
-      // allow auto import and register components used in markdown
-      customLoaderMatcher: id => id.endsWith('.md'),
-
-      // auto import icons
+    viteComponents({
       customComponentResolvers: [
         // https://github.com/antfu/vite-plugin-icons
         ViteIconsResolver({
           componentPrefix: '',
-          // enabledCollections: ['carbon']
+          enabledCollections: [
+            // Core UI Brands (https://icones.js.org/collection/cib)
+            'cib',
+            // Heroicons Outline (https://icones.js.org/collection/heroicons-outline)
+            'heroicons-outline',
+            // Heroicons Solid (https://icones.js.org/collection/heroicons-solid)
+            'heroicons-solid',
+          ],
         }),
+
+        // import headlessui components (https://headlessui.dev)
+        (name) => {
+          if (name.startsWith('HeadlessUI.'))
+            return { importName: name.replace(/^HeadlessUI\./, ''), path: '@headlessui/vue' };
+        },
       ],
     }),
 
-    // https://github.com/antfu/vite-plugin-icons
-    ViteIcons(),
-
-    // https://github.com/antfu/vite-plugin-windicss
-    WindiCSS({
-      safelist: 'prose prose-sm m-auto text-left',
-    }),
-
-    // https://github.com/antfu/vite-plugin-pwa
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-
     // https://github.com/intlify/vite-plugin-vue-i18n
-    VueI18n({
+    vueI18n({
       include: [path.resolve(__dirname, 'locales/**')],
     }),
   ],
+
   // https://github.com/antfu/vite-ssg
   ssgOptions: {
     script: 'async',
@@ -115,4 +75,4 @@ export default defineConfig({
       'vue-demi',
     ],
   },
-})
+});
