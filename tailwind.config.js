dbee2595017ebe2788bb71613f85b460d3427712
Joe Pantazidis <joe.pantazidis@outlook.de>
2021-04-21 10:51:53 +0200

    add tailwind plugins typography and aspect-ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,53 @@
 const colors = require('tailwindcss/colors');
+const typography = require('@tailwindcss/typography');
+const aspectRatio = require('@tailwindcss/aspect-ratio');
 
 module.exports = {
   mode: 'jit',
-  purge: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-  ],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './index.html',
+      './src/**/*.{vue,js,ts,jsx,tsx}',
+    ],
+    options: {
+      safelist: ['prose', 'prose-sm', 'm-auto'],
+    },
+  },
   darkMode: 'class', // or 'media' or false
   theme: {
     colors,
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: 'inherit',
+            a: {
+              color: 'inherit',
+              opacity: 0.75,
+              '&:hover': {
+                opacity: 1,
+                color: colors.teal[600],
+              },
+            },
+            b: { color: 'inherit' },
+            strong: { color: 'inherit' },
+            em: { color: 'inherit' },
+            h1: { color: 'inherit' },
+            h2: { color: 'inherit' },
+            h3: { color: 'inherit' },
+            h4: { color: 'inherit' },
+            code: { color: 'inherit' },
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    aspectRatio,
+    typography,
+  ],
 };
