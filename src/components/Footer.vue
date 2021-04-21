6fcd561c5bf6effd3172154e1cef46cf3aad23a0
Joe Pantazidis <joe.pantazidis@outlook.de>
2021-04-21 09:57:26 +0200

    remove about page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { useI18n } from 'vue-i18n'
-import { isDark, toggleDark } from '~/logic'
+import { useI18n } from 'vue-i18n';
+import { isDark, toggleDark } from '~/logic';
 
-const { t, availableLocales, locale } = useI18n()
+const { t, availableLocales, locale } = useI18n();
 
 const toggleLocales = () => {
   // change to some real logic
-  const locales = availableLocales
-  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
-}
+  const locales = availableLocales;
+  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length];
+};
 </script>
 
 <template>
@@ -25,10 +25,6 @@ const toggleLocales = () => {
     <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
       <carbon-language />
     </a>
-
-    <router-link class="icon-btn mx-2" to="/about" :title="t('button.about')">
-      <carbon-dicom-overlay />
-    </router-link>
 
     <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
       <carbon-logo-github />
